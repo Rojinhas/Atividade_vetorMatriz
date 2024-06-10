2988cef2ec6282b257ff6a8b38d4836fd0148091
rojinhas <ruthrojasdasilva12@gmail.com>
2024-06-10 12:00:27 -0300

    Atividade Vetores e Matriz

---
+++ src/vetoresematriz/arrayVetores.java
@@ -0,0 +1,10 @@
+package vetoresematriz;
+
+public class arrayVetores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/vetoresematriz/arraymatrizes.java
@@ -0,0 +1,17 @@
+package vetoresematriz;
+
+import java.util.Scanner;
+
+public class arraymatrizes {
+
+	public static void main(String[] args) {
+		
+		int[] numeros = {2,5,1,3,4,9,7,8,10,6};
+		
+		Scanner leia = new Scanner (System.in);
+		
+		System.out.println("Digite o número que deseja encontrar: ");
+		String numeroEscolhido = leia.next();
+		
+		}
+	}
