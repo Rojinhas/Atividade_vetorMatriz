151c74fd54d61efca9b277d3d3a9d143631e840b
rojinhas <ruthrojasdasilva12@gmail.com>
2024-06-10 13:17:17 -0300

    Atividade vetores e matrizes

--- src/vetoresematriz/arrayVetores.java
+++ src/vetoresematriz/arrayVetores.java
@@ -1,10 +1,30 @@
 package vetoresematriz;
 
+import java.util.Scanner;
+
 public class arrayVetores {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner leia = new Scanner(System.in);
 
+		int numeros[] = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+		boolean achou = false;
+
+		System.out.println("Digite o número que deseja encontrar: ");
+		int numeroProcurado = leia.nextInt();
+
+		for (int i = 0; i < numeros.length; i++) {
+
+			if (numeros[i] == numeroProcurado) {
+				achou = true;
+				System.out.println("O numero " + numeroProcurado + "está localizado na posição: " + i);
+				break;
+
+			}
+		}
+		if (achou == false) {
+			System.out.println("O numero não foi encontrado");
+		}
 	}
 
 }
--- src/vetoresematriz/arraymatrizes.java
+++ src/vetoresematriz/arraymatrizes.java
@@ -5,13 +5,45 @@ import java.util.Scanner;
 public class arraymatrizes {
 
 	public static void main(String[] args) {
-		
-		int[] numeros = {2,5,1,3,4,9,7,8,10,6};
-		
-		Scanner leia = new Scanner (System.in);
-		
-		System.out.println("Digite o número que deseja encontrar: ");
-		String numeroEscolhido = leia.next();
-		
+
+		int[][] matriz = new int[3][3];
+		int somaPrincipal = 0, somaSecundaria = 0, linha = 0, coluna = 0;
+		String diagonalPrincipal = "", diagonalSecundaria = "";
+
+		Scanner leia = new Scanner(System.in);
+
+		for (linha = 0; linha < matriz.length; linha++) {
+
+			for (coluna = 0; coluna < matriz.length; coluna++) {
+
+				System.out.println("Digite o numero da posição: (" + linha + "," + coluna + "): ");
+				matriz[linha][coluna] = leia.nextInt(); // Guardar infos -> []
+			}
+
 		}
+
+		for (int i = 0; i < matriz.length; i++) {
+			somaPrincipal = somaPrincipal + matriz[i][i];
+			diagonalPrincipal = diagonalPrincipal + matriz[i][i] + " ";
+		}
+
+		for (int i = 0; i < matriz.length; i++) {
+			somaSecundaria = somaSecundaria + matriz[i][matriz.length - 1 - i];
+			diagonalSecundaria = diagonalSecundaria + matriz[i][matriz.length - 1 - i] + " ";
+
+		}
+
+		System.out.println("\n\nSoma de todos Elementos da Diagonal Principal:\n");
+		System.out.println(somaPrincipal);
+
+		System.out.println("\n\nSoma de todos Elementos da Diagonal Secundária:\n");
+		System.out.println(somaSecundaria);
+
+		System.out.println("\n\nElementos da Diagonal Principal:\n");
+		System.out.println(diagonalPrincipal);
+
+		System.out.println("\n\nElementos da Diagonal Secundária:\n");
+		System.out.println(diagonalSecundaria);
+		leia.close();
 	}
+}
